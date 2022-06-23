23ef4cb904c1cb4157c962866eca58c6299ecc7e
Conrad <jude@imagefactor.com.ng>
2022-06-23 16:49:45 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,28 @@
+var operator = (prompt('Enter Operator (-, +, /, *)'));
+
+
+var num1 = window.prompt('Enter a number');
+var num2 = window.prompt('Enter a number');
+
+let result;
+
+
+document.write(num1 + num2);
+
+if (operator == '+') {
+    result =- num1 + num2;
+}
+
+else if (operator == '-') {
+    result = num1 - num2;
+}
+
+else if ( operator == '/') {
+    result = num1 / num2;
+}
+
+else (operator == '*'); {
+    result = num1 * num2;
+}
+
+alert.result(Number) 
